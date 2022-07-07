922af265794ca84110a47157a73e30fb03d57184
Clyde-Goodall <goodalluigi@gmail.com>
2022-07-06 20:59:45 -0400

    idk 2

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,31 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  dev: {
-    require('dotenv').config({ path: `./.env.${mode}` });
-    const isProd = mode === 'production';
-    return {
-      plugins: [vue()],
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('BarChart')
-        }
+
+  require('dotenv').config({ path: `./.env.${mode}` });
+  const isProd = mode === 'production';
+  return {
+    plugins: [vue()],
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith('BarChart')
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://165.227.220.80:3002',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        }      
       },
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      // server: {
-      //   proxy: {
-      //     '/api': {
-      //       target: 'https://165.227.220.80:3002',
-      //       changeOrigin: true,
-      //       secure: false,
-      //       ws: true,
-      //     }      
-      //   },
-      // },
-    }
+    },
   }
+
 })
